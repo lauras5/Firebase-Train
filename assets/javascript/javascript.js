357033cec69339ac3e92e02d1bb7ff0f3fb71b53
Laura Salcido <idlaurasalcido@gmail.com>
2018-03-02 17:23:33 -0800

    working on math to calculate next train arrival/minutes to train

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,75 +3,93 @@ $(document).ready(function() {
   var tName
   var tDestination
   var tFrequency
-  var tArrival
-  var nextArrival
-  var minutesToTrain
+  var tArrival //first time
+  var nextArrival //next time tArrival + Frequency that hasnt passed yet
+  var minutesToTrain//diff in minutes between now and nextArrival
   var tList
-  var tdeparture
-  var now = moment().format('MMMM Do YYYY, LT');
-  console.log(now)
+  //don't need date, there for aesthetics
+  var date = moment().format('MMMM Do YYYY');
+  $("#day").html(date)
+  //adding time to page
+  var now = moment().format('LT')
   $("#time").html(now)
-
   
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyDMzQJYRc3YYs8XXXzcff-8DVu8XKUpvl8",
-      authDomain: "trains-55.firebaseapp.com",
-      databaseURL: "https://trains-55.firebaseio.com",
-      projectId: "trains-55",
-      storageBucket: "",
-      messagingSenderId: "908445882136"
-    };
 
-    firebase.initializeApp(config);
-
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDMzQJYRc3YYs8XXXzcff-8DVu8XKUpvl8",
+    authDomain: "trains-55.firebaseapp.com",
+    databaseURL: "https://trains-55.firebaseio.com",
+    projectId: "trains-55",
+    storageBucket: "",
+    messagingSenderId: "908445882136"
+  };
+  
+  firebase.initializeApp(config);
+  
   //access to database
   var database =  firebase.database()
   //acces to trains
   var trains = database.ref('/trains')
   console.log('hello')
-
+  
   $(".submitBTN").on("click", function(){
     tName = $("#tName").val().trim()
-    console.log(tName)
     tDestination = $("#tDestination").val().trim()
     tFrequency = parseInt($("#tFrequency").val().trim())
-    tArrival = parseInt($("#tArrival").val().trim())
-    console.log(tDestination)
-    console.log(tName)
-    //append train name to 
+    tArrival = $("#tArrival").val().trim()
+    console.log(tArrival)
+    var firstTimeConverted = moment(tArrival,"hh:mm").subtract(1, "years")
+    console.log(firstTimeConverted)
+    console.log(now)
+        nextArrival = '05:30' //every 15 minutes starting with tArrival,
 
+    console.log(tFrequency)
+    minutesToTrain = moment(now).to(nextArrival)
+    console.log(minutesToTrain)
+    // console.log(tDestination)
+    // console.log(tName)
+    //append train name to 
+    
     trains.push({
       name : tName,
       destination : tDestination,
       frequency : tFrequency,
-      arrival : tArrival
+      arrival : tArrival,
     })
     event.preventDefault()
   })
-
-  // trains.on('child_added', function(snap) {
-  //   console.log(snap.val())
-  // })
   
-  trains.orderByChild('number',).limitToLast(6).on('child_added', function(snap) {
-    console.log(snap.val())
-    tList = $("<div class='card'><div class='card-body'><h3 class='card-title'>"+snap.val().name+"</h3><p class='card-text'>"+snap.val().destination+"</p><p class='card-text'>"+snap.val().frequency+"</p><p class='card-text'>"+snap.val().arrival+"</p><a href='https://www.amtrak.com/home.html' class='card-link'>Buy A Ticket</a></div></div>")
-
-    $("#currentTrainTimes").append(tList)
-
+    trains.orderByChild('number',).limitToLast(12).on('child_added', function(snap) {
+      console.log(snap.val())
+      var t = snap.val()
+      
+      //can split up but there is no need
+      tList = $("<div class='card'><div class='card-body'><h3 class='card-title' id='card-name'>"+t.name+"</h3><p class='card-text' id='card-destination'>"+t.destination+"</p><p class='card-text' id='card-frequency'>"+t.frequency+"</p><p class='card-text' id='card-next-arrival'>"+t.Arrival+"</p><p class='card-text' id='card-minutes'>"+minutesToTrain+"</p><a href='https://www.amtrak.com/home.html' class='card-link'>Buy A Ticket</a></div></div>")
+      $("#currentTrainTimes").append(tList)  
+    })
+    //   console.log(snap.val())
     
-  })
-  //for current trains
+    $('#timeBtn').on("click", function() {
+      now = moment().format('LT')
+      $("#time").html(now)
+      console.log(now)
+      //update train info
+    })
+    //for current trains
   //add title
   //append list array for trains
   //list contains train name, destination, frequency, next arrival & minutes away
   
-  //add form for new trains
   //train name/destination/frequency and first arrival
   //on click event for .submit button
   //append new train to current trains
   //clear input fields
   //add to array
   
+  // start time = tArrival
+  // for tArrival add tfrequency 
+  // if this this time > now
+  // write next time
+  // calc difference in minutes until next time
 })
